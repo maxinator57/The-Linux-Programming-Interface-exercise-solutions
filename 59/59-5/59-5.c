dbbcf6f26512385c9784a227f0f1778dc39cdaaa
Max Urmanov <justmax57@yandex.ru>
2024-10-14 16:29:05 +0300

    improve solution of ex. 59-5

--- 59/59-5/59-5.c
+++ 59/59-5/59-5.c
@@ -1,5 +1,7 @@
 #define _GNU_SOURCE
 
+
+#include <assert.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -8,11 +10,13 @@
 #include <sys/socket.h>
 
 
-struct sockaddr_in6 CreateIpV6LocalhostSocketAddr(const in_port_t port) {
+struct sockaddr_in6 CreateIpV6LocalhostSocketAddr(
+    const in_port_t port /* in host byte order */
+) {
     struct sockaddr_in6 socketAddr;
     memset(&socketAddr, 0, sizeof(socketAddr));
     socketAddr.sin6_family = AF_INET6;
-    socketAddr.sin6_port = port;
+    socketAddr.sin6_port = htons(port);
     socketAddr.sin6_addr = in6addr_loopback;
     return socketAddr;
 }
@@ -22,7 +26,8 @@ int main() {
     // Create `sockaddr_in` structs with addresses
     // for the first and second sockets.
     const struct sockaddr_in6 firstSocketAddr = CreateIpV6LocalhostSocketAddr(40000),
-                             secondSocketAddr = CreateIpV6LocalhostSocketAddr(50000);
+                             secondSocketAddr = CreateIpV6LocalhostSocketAddr(50000),
+                              thirdSocketAddr = CreateIpV6LocalhostSocketAddr(60000);
 
     // Create the file descriptor for the first socket.
     const int firstSocketFd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
@@ -31,7 +36,7 @@ int main() {
         printf("(error code: %s)\n", strerrorname_np(errno));
         exit(EXIT_FAILURE);
     }
-    // Bind the first socket file descriptor to the first socket address.
+    // Bind the first socket to the first socket address.
     if (bind(
         firstSocketFd,
         (const struct sockaddr*) &firstSocketAddr,
@@ -81,21 +86,108 @@ int main() {
         printf("(error code: %s)\n", strerrorname_np(errno));
         exit(EXIT_FAILURE);
     }
+    // Bind the third socket file descriptor to the third socket address.
+    if (bind(
+        thirdSocketFd,
+        (const struct sockaddr*) &thirdSocketAddr,
+        sizeof(thirdSocketAddr)
+    ) == -1) {
+        perror("bind() syscall for the third socket failed with the following error");
+        printf("(error code: %s)\n", strerrorname_np(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Define a datagram.
+    const char datagram = 'x';
+
+    // Try to send a datagram from the second socket to the first socket.
+    {
+        const ssize_t nBytesSent = sendto(
+            secondSocketFd,
+            &datagram,
+            1,
+            0,
+            (const struct sockaddr*) &firstSocketAddr,
+            sizeof(firstSocketAddr)
+        );
+        if (nBytesSent == -1) {
+            perror(
+                "sendto() syscall from the third socket to the first "
+                "socket failed with the following error"
+            );
+            printf("(error code: %s)\n", strerrorname_np(errno));
+        } else {
+            printf(
+                "sendto() syscall successfully sent %ld byte(s) "
+                "from the third socket to the first socket\n",
+                nBytesSent
+            );
+        }
+    }
 
     // Try to send a datagram from the third socket to the first socket.
-    const char datagram = 'x';
-    if (sendto(
-        thirdSocketFd,
-        &datagram,
-        1,
-        0,
-        (const struct sockaddr*) &firstSocketAddr,
-        sizeof(firstSocketAddr)
-    ) == -1) {
-        perror(
-            "sendto() from the third socket to the first "
-            "socket failed with the following error"
+    {
+        const ssize_t nBytesSent = sendto(
+            thirdSocketFd,
+            &datagram,
+            1,
+            0,
+            (const struct sockaddr*) &firstSocketAddr,
+            sizeof(firstSocketAddr)
         );
-        printf("(error code: %s)\n", strerrorname_np(errno));
+        if (nBytesSent == -1) {
+            perror(
+                "sendto() syscall from the third socket to the first "
+                "socket failed with the following error"
+            );
+            printf("(error code: %s)\n", strerrorname_np(errno));
+        } else {
+            printf(
+                "sendto() syscall successfully sent %ld byte(s) "
+                "from the third socket to the first socket\n",
+                nBytesSent
+            );
+        }
+    }
+
+    // Try to receive the datagrams from the second and third socket on the first socket.
+    struct sockaddr_in6 senderAddr;
+    socklen_t senderAddrSize = sizeof(senderAddr);
+    char incomingDatagram;
+    for (int i = 0; i != 2; ++i) { // try to two datagrams
+        const ssize_t nBytesReceived = recvfrom(
+            firstSocketFd,
+            &incomingDatagram,
+            1,
+            MSG_DONTWAIT, // make this call non-blocking
+            (struct sockaddr*) &senderAddr,
+            &senderAddrSize
+        );
+        if (nBytesReceived == -1) {
+            perror(
+                "recvfrom() syscall on the first socket "
+                "failed with the following error"
+            );
+            printf("(error code: %s)\n", strerrorname_np(errno));
+            exit(EXIT_FAILURE);
+        } else {
+            assert(senderAddr.sin6_family == AF_INET6);
+            printf(
+                "recvfrom() syscall on the first socket successfully received %ld byte(s).\n"
+                "Sender address:\n"
+                "    Family: %d\n"
+                "    Port: %d\n"
+                "    IP address: ",
+                nBytesReceived,
+                senderAddr.sin6_family,
+                ntohs(senderAddr.sin6_port)
+            );
+            const uint8_t* senderIpV6Addr = (const uint8_t *) &senderAddr.sin6_addr;
+            printf("%u", senderIpV6Addr[0]);
+            for (size_t i = 1; i < sizeof(senderAddr.sin6_addr); ++i) {
+                printf(":%u", senderIpV6Addr[i]);
+            }
+            puts("");
+        }
     }
 }
